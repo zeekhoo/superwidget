73f2abe74e1c09cb8430a426785da4ca345f6918
lee-tschetter <37558979+lee-tschetter@users.noreply.github.com>
2018-03-28 17:27:28 -0500

    Update oidc_base.js (#1)

--- static/js/oidc_base.js
+++ static/js/oidc_base.js
@@ -1,4 +1,4 @@
-var oktaSignIn = new OktaSignIn({
+var oktaSignIn = new OktaSignIn({ //Do not edit this section.
     baseUrl: 'https://[[org]]',
     clientId: '[[aud]]',
     redirectUri: '[[redirect]]',
@@ -8,26 +8,32 @@ var oktaSignIn = new OktaSignIn({
         scopes: [[scopes]],
     },
     features: {
-        router: true,
-        registration: false,
-        //rememberMe: true,
-        //multiOptionalFactorEnroll: true,
-        //selfServiceUnlock: true,
-        //smsRecovery: true,
-        //callRecovery: true,
+    //These affect the functionality of the widget and will enable or disable capabilities
+    //Detailed feature information can be found here: https://github.com/okta/okta-signin-widget#okta-sign-in-widget 
+        router: true, //Leave this set to true for the API demo
+        registration: false, //Enable self-service registration flow
+        rememberMe: true, //Setting to false will remove the checkbox to save username
+        //multiOptionalFactorEnroll: true, //Allow users to enroll in multiple optional factors before finishing the authentication flow. 
+        //selfServiceUnlock: true, //Will enable unlock in addition to forgotten password
+        //smsRecovery: true, //Enable SMS-based account recovery
+    	//callRecovery: true, //Enable voice call-based account recovery
     },
-    logo: '/static/img/gear_half.png',
-    language: 'en',
+    // Look and feel changes
+    logo: '/static/img/gear_half.png', //Other included logos: logo_widgico.png, gear_logo.png, gear_half.png
+    language: 'en', //Suggested languages include fr, de, es 
     i18n: {
+    //Customizes the text for the widget when using English. Additional sections can be added to customize other languages. 
         'en': {
-            'primaryauth.title': 'Sign In',
-            'primaryauth.submit': 'Sign In',
+            'primaryauth.title': 'Sign In', //Changes the sign in text
+            'primaryauth.submit': 'Sign In', //Changes the sign in button
+            //'primaryauth.username.tooltip': 'Enter your APIDemo ID', //Changes the tooltip for username
+            //'primaryauth.password.tooltip': 'Your APIDemo Password', //Changes the tooltip for password
             //Example more tags: [primaryauth.username.placeholder,  primaryauth.password.placeholder, needhelp, etc.]
             //Full list here: https://github.com/okta/okta-signin-widget/blob/master/packages/@okta/i18n/dist/properties/login.properties
         }
     },
 });
-oktaSignIn.renderEl(
+oktaSignIn.renderEl( //Do not edit this section.
     {el: '#okta-login-container'},
     function (res) {
         var key = '';
