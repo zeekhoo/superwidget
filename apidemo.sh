dcbff3fa9ba3bef28086e1ecf2b47ca516aba2cc
lee-tschetter <37558979+lee-tschetter@users.noreply.github.com>
2018-03-28 18:23:46 -0500

    Update apidemo.sh

--- apidemo.sh
+++ apidemo.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 # This script can be used to simplify starting and stopping the API Demo Docker container.
-# ./apidemo.sh start
-# ./apidemo.sh stop
-# ./apidemo.sh status
-# ./apidemo.sh cleanup
 
 SERVER_CONTAINER="zzkhoo/okta-api-demo:latest"
 PWD=$(pwd)
@@ -28,23 +24,35 @@ case "$1" in
     start)
     	CONTAINER_ID=$(docker ps -a | grep -v Exit | grep $SERVER_CONTAINER | awk '{print $1}')
     	if [[ -z $CONTAINER_ID ]] ; then
-    		echo "Starting Docker API Demo"
-        	docker run -p 8000:8000 --env-file=env.list -v $STATIC_PATH:$DOCKER_STATIC_PATH -t $SERVER_CONTAINER
+    		echo "----- Starting Docker API Demo as a background process. Docker will continue to run until you stop it. -----"
+        	docker run -p 8000:8000 --env-file=env.list -v $STATIC_PATH:$DOCKER_STATIC_PATH -d $SERVER_CONTAINER
         	getStatus
     	else
         	getStatus
     	fi
         ;;
-
-    status)
+        
+	interactive)
+    	CONTAINER_ID=$(docker ps -a | grep -v Exit | grep $SERVER_CONTAINER | awk '{print $1}')
+    	if [[ -z $CONTAINER_ID ]] ; then
+    		echo "----- Starting Docker API Demo in interactive mode, hitting ctrl-c will stop the Docker process. -----"
+        	docker run -p 8000:8000 --env-file=env.list -v $STATIC_PATH:$DOCKER_STATIC_PATH -it $SERVER_CONTAINER
+        	getStatus
+    	else
+        	getStatus
+    	fi
+        ;;
+        
+	status)
         getStatus
         ;;
 
-    cleanup)
-	echo "Cleaning up all inactive and stopped Docker containers"
-	docker ps -aq --no-trunc | xargs docker rm
-	;;
-    stop)
+	cleanup)
+		echo "Cleaning up all inactive and stopped Docker containers"
+		docker ps -aq --no-trunc | xargs docker rm
+		;;
+		
+	stop)
         CONTAINER_ID=$(docker ps -a | grep -v Exit | grep $SERVER_CONTAINER | awk '{print $1}')
         if [[ -n $CONTAINER_ID ]] ; then
             SRV=$(docker stop $CONTAINER_ID)
@@ -58,7 +66,12 @@ case "$1" in
         ;;
 
     *)
-        echo "Usage: `basename $0`  {start|stop|status|cleanup}"
+        echo "Usage: ./`basename $0`  {start|stop|status}"
+        echo "./`basename $0` start		-- Starts the API container as a background process that will run until stopped"
+		echo "./`basename $0` stop 		-- Stops the API Demo container"
+		echo "./`basename $0` status		-- Shows if the API Demo is currently running"
+		echo "./`basename $0` cleanup		-- Cleans up any old, non-running Docker containers"
+		echo "./`basename $0` interactive		-- Runs Docker in interactive mode which terminates when the script is terminated"
         exit 1
         ;;
 esac
