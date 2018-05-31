f5ec5932b50bb8e85e716c0b9ee0f7f33e73fb19
zeekhoo-okta <zeekhoo@gmail.com>
2018-05-30 17:45:39 -0700

    use auth-js automatic token renewal

--- static/js/show_tokens.js
+++ static/js/show_tokens.js
@@ -1,43 +1,49 @@
-var url = 'https://' + org;
+function showToken(org, aud, iss, uri) {
+    var baseUrl = 'https://' + org;
+    var issuer = baseUrl + '/oauth2/' + iss;
 
-var oktaSignIn = new OktaSignIn({
-    baseUrl: url
-});
+    var oktaSignIn = new OktaSignIn({
+        baseUrl: baseUrl,
+        clientId: aud,
+        redirectUri: uri,
+        authParams: {
+            issuer: issuer
+        }
+    });
 
-oktaSignIn.session.get(function (res) {
-  console.log(res);
-  if (res.status === 'ACTIVE') {
-    console.log('have session');
-    console.log('user_id = ' + res.userId);
+    oktaSignIn.session.get(function (res) {
+      console.log(res);
+      if (res.status === 'ACTIVE') {
+        console.log('have session');
+        console.log('user_id = ' + res.userId);
 
-    var id_token = srv_id_token;
-    if (oktaSignIn.tokenManager.get('idToken')) {
-        id_token = oktaSignIn.tokenManager.get('idToken').idToken;
-    }
-    var access_token = srv_access_token;
-    if (oktaSignIn.tokenManager.get('accessToken')) {
-        var access_token = oktaSignIn.tokenManager.get('accessToken').accessToken;
-    }
-    if (id_token != '') {
-        var id_token_parts = id_token.split('.');
-        document.getElementById('id_token').innerHTML = id_token;
-        document.getElementById('id_token_header').innerHTML = prettyPrint(window.atob(id_token_parts[0]));
-        document.getElementById('id_token_decoded').innerHTML = prettyPrint(window.atob(id_token_parts[1]));
-    }
-    if (access_token != '') {
-        var access_token_parts = access_token.split('.');
-        document.getElementById('access_token').innerHTML = access_token;
-        document.getElementById('access_token_header').innerHTML = prettyPrint(window.atob(access_token_parts[0]));
-        document.getElementById('access_token_decoded').innerHTML = prettyPrint(window.atob(access_token_parts[1]));
-    }
-  }
-});
+        if (oktaSignIn.tokenManager.get('idToken')) {
+            populateFields('id_token', oktaSignIn.tokenManager.get('idToken').idToken);
+        } else {
+            populateFields('id_token', srv_id_token);
+        }
+        if (oktaSignIn.tokenManager.get('accessToken')) {
+            populateFields('access_token', oktaSignIn.tokenManager.get('accessToken').accessToken);
+        } else {
+            populateFields('access_token', srv_access_token);
+        }
+      }
+    });
+}
 
+
+function populateFields(elementId, token) {
+    var token_parts = token.split('.');
+    document.getElementById(elementId).innerHTML = token;
+    document.getElementById(elementId + '_header').innerHTML = prettyPrint(window.atob(token_parts[0]));
+    document.getElementById(elementId + '_decoded').innerHTML = prettyPrint(window.atob(token_parts[1]));
+}
 
 function prettyPrint(ugly) {
     var obj = JSON.parse(ugly);
     return JSON.stringify(obj, undefined, 4);
 }
+
 
 
 
